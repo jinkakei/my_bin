429fb98f23ed80baf17ceff3c7c67b118154c165
Kei Nishina <k247loveke@gmail.com>
2015-09-28 17:27:00 +0859

    add make_with_log.rb

---
+++ make_with_log.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+if ARGV[0] != nil
+  log_fname = ARGV[0]
+else
+  log_fname = "make.log"
+end
+
+cmd_str = "make 2>&1 | tee log_fname"
+puts "ruby info: #{cmd_str}"
+#ret = system( cmd_str )
+puts "ruby info: ret = #{ret}"
