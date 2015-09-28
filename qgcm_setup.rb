3bae459a8630ded13d0ceef452323ae2402fb206
Kei Nishina <k247loveke@gmail.com>
2015-09-28 18:36:23 +0859

    add qgcm_setup.rb

---
+++ qgcm_setup.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+#target_dir = "../src/"
+target_dir = "../src_test29/"
+work_dir = "."
+files = ["*.F", "*.f", "input.params", \
+         "make.macro", "make.config", "Makefile", \
+         "fftpack/" ]
+
+#=begin
+files.each do | f |
+  cmd_str =  "ln -s #{target_dir}#{f} #{work_dir}"
+  p cmd_str
+  ret = system( cmd_str )
+end
+#=end
+
+
