138fe3297be84af0eb9303f3d2b7ced59868fc60
Kei Nishina <k247loveke@gmail.com>
2015-10-03 16:40:43 +0859

    add output codes by gen_~.rb

--- get_qgcm_params.F90
+++ get_qgcm_params.F90
@@ -3,10 +3,17 @@
 program get_parameters 
       use parameters
       implicit none
-
+! this codes made by gen_get_qgpara.rb
+!   match indent: select lines (Shift + V) -> type =
       write(*,*) 
       write(*,*) "nxto = ", nxto
-
+      write(*,*) "nyto = ", nyto
+      write(*,*) "nxpo = ", nxpo
+      write(*,*) "nypo = ", nypo
+      write(*,*) "nlo = ", nlo
+      write(*,*) "dxo = ", dxo
+      write(*,*) "gpoc = ", gpoc
+      write(*,*) "hoc = ", hoc
 
 end program get_parameters
 
