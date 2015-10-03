f5d9e83b137e090c99708dbb4df1fcdf9f35d496
Kei Nishina <k247loveke@gmail.com>
2015-10-03 16:28:39 +0859

    remove test codes from get_qgcm_params.F90

--- get_qgcm_params.F90
+++ get_qgcm_params.F90
@@ -1,12 +1,8 @@
 ! create: 2015-09-30
 
 program get_parameters 
-!      use parameters
+      use parameters
       implicit none
-
-      integer, parameter:: nxto=960 ! temporary for test
-
-      write(*,*) "call test of fortran progam from ruby script"
 
       write(*,*) 
       write(*,*) "nxto = ", nxto
