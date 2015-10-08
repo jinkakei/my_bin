101320d093d93bb9628e35be9162f5600147bc16
Kei Nishina <k247loveke@gmail.com>
2015-10-08 11:56:02 +0900

    modify apt_upgrades_k247.rb

--- apt_upgrades_k247.rb
+++ apt_upgrades_k247.rb
@@ -5,10 +5,12 @@ time_str = Time.now.strftime("%Y%m%d_%H%M_%S")
 upd_log = "#{log_dir}apt-get_update.#{time_str}" 
 upg_log = "#{log_dir}apt-get_upgrade.#{time_str}" 
 
+upd_cmd = "sudo apt-get update 2>&1 | tee #{upd_log}"
+upg_cmd = "sudo apt-get upgrade 2>&1 | tee #{upg_log}"
+
 if File::exist?( log_dir )
-# ? sudo
-  system("sudo apt-get update 2>&1 | tee #{upd_log}")
-  system("sudo apt-get upgrade 2>&1 | tee #{upg_log}")
+  puts(upd_cmd); system(upd_cmd)
+  puts(upg_cmd); system(upg_cmd)
 else
   puts "Error, no log_dir. make #{log_dir}"
 end
