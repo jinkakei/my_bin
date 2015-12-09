b4d3866f6f7047f96f1e59b559d2f105f06d4777
Kei Nishina <k247loveke@gmail.com>
2015-12-09 19:38:24 +0900

    make_with_log.rb: trivial update

--- make_with_log.rb
+++ make_with_log.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/ruby
 
+# ToDo
+#   - make function? (2015-12-09)
+#   - 
+
 if ARGV[0] != nil
   log_fname = ARGV[0]
 else
@@ -10,3 +14,4 @@ cmd_str = "make 2>&1 | tee #{log_fname}"
 puts "ruby info: #{cmd_str}"
 ret = system( cmd_str )
 puts "ruby info: ret = #{ret}, logfile = #{log_fname}"
+system( "echo \"command: #{cmd_str}\" >> #{log_fname}" )
